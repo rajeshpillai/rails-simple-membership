0666845b13f3d2d2dde0590ac7cbf4e8abeb7cc5
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-14 17:23:21 +0530

    Prettier routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'home/index'
+  #get 'sessions/new'
+  #get 'home/index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   resources :sessions
   resources :users
   root to: 'home#index'
