ed7c00ae0529631c57e3b5396414eb0ea7c8b048
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-14 16:31:09 +0530

    Added accessor and validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password  # simple auth support for model using password_digest column
+
+  attr_accessible :email, :password, :password_confirmation
+
+  validates_uniqueness_of :email
+
+  #NOTE
+  # We don’t need to validate the presence of the password and password_confirmation fields, 
+  #however, as this will be handled by has_secure_password.
 end
