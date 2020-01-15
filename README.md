c25ebc0ac1eed0cbc1dec8600c8e059f762f0561
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-15 11:05:31 +0530

    Added creating random string for auth

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# TODO
+Following features are covered
+
+* Register user
+* Custom user login
+* Logout
+* Showing validation messages
+* Remember me and reset password
+
+
 # README
 
 This README would normally document whatever steps are necessary to get the
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,23 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 8}, allow_nil: true
   #attr_accessor :email, :password, :password_confirmation (throws error)
 
+  # To create the token we use ActiveSupport’s 
+  # SecureRandom class to generate a random string. 
+  # We check that no other user exists with that token and 
+  # repeatedly generate another random token while this is true. 
+  # We call the method in a before_create filter so that the 
+  # token is generated when a new user is saved for the first time. 
+  # If we have pre-existing users in the database we’ll need to create 
+  # tokens for them as we could create a rake task to do this, though 
+  # we won’t do so here.
 
+  before_create { generate_token(:auth_token)}
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 
   #NOTE
   # We don’t need to validate the presence of the password and password_confirmation fields, 
