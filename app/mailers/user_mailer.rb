b90259dbe5efba3a65f2d3512e9c616889a20d65
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-15 12:24:24 +0530

    Fix error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,6 @@ class UserMailer < ApplicationMailer
 
   def password_reset (user)
     @user = user
-    mail to: => user.email, :subject => "Password Reset"
+    mail :to => user.email, :subject => "Password Reset"
   end
 end
